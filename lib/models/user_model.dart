03566bb29b7ae59920750ee1758c3b0842b80aaf
Abdoulaye Chérif Touré <toure_cherif94@yahoo.fr>
2025-03-10 23:35:19 +0100

    feat: profile screen

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,31 +1,45 @@
+import 'package:bit_money/models/pdv_model.dart';
+
 class UserModel {
   final String name;
   final String email;
   final String id;
   final String role;
+  final String accountType;
+  final int commission;
+  PDV? pdv;
 
   UserModel({
     required this.name,
     required this.email,
     required this.id,
     required this.role,
+    required this.accountType,
+    required this.commission,
+    this.pdv,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
+      id: json['id'] ?? '',
       name: json['name'] ?? '',
       email: json['email'] ?? '',
-      id: json['id'] ?? '',
       role: json['role'] ?? '',
+      accountType: json['accountType'] ?? '',
+      commission: json['commission'] ?? '',
+      pdv: json['pdv'] != null ? PDV.fromJson(json['pdv']) : null,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'email': email,
-      'id': id,
       'role': role,
+      'accountType': accountType,
+      'commission': commission,
+      'pdv': pdv?.toJson(),
     };
   }
 }
