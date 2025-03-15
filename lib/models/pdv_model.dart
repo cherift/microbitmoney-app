9f0055770c41aeb710dfeef7822f4424b0a2805a
Abdoulaye Chérif Touré <toure_cherif94@yahoo.fr>
2025-03-15 09:21:39 +0100

    Disabling erollment button when we are not admin

--- lib/models/pdv_model.dart
+++ lib/models/pdv_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class PDV {
   final String id;
   final String name;
@@ -51,5 +53,36 @@ class PDV {
       'createdAt': createdAt.toIso8601String(),
       'updatedAt': updatedAt.toIso8601String(),
     };
+  }
+
+  bool isOpen() {
+    try {
+      final now = DateTime.now();
+      final currentTime = TimeOfDay(hour: now.hour, minute: now.minute);
+
+      final openingParts = this.openingTime.split(':');
+      final closingParts = this.closingTime.split(':');
+
+      final openingTime = TimeOfDay(
+        hour: int.parse(openingParts[0]),
+        minute: int.parse(openingParts[1])
+      );
+
+      final closingTime = TimeOfDay(
+        hour: int.parse(closingParts[0]),
+        minute: int.parse(closingParts[1])
+      );
+
+      final isWeekend = now.weekday == DateTime.saturday || now.weekday == DateTime.sunday;
+
+      final currentMinutes = currentTime.hour * 60 + currentTime.minute;
+      final openingMinutes = openingTime.hour * 60 + openingTime.minute;
+      final closingMinutes = closingTime.hour * 60 + closingTime.minute;
+
+      return (!isWeekend || openWeekend)
+        && (currentMinutes >= openingMinutes && currentMinutes < closingMinutes);
+    } catch (e) {
+      return false;
+    }
   }
 }
